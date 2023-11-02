87f6586b6d2d187077e30a6424007d90d00f02bd
Dhyeonle9 <dhyeon.le9@gmail.com>
2023-11-02 14:38:35 +0900

    create기능 구현

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
 
 def index(request):
@@ -16,3 +16,18 @@ def detail(request, id):
         'article': article,
     }
     return render(request, 'detail.html', context)
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+
+    article = Article()
+    article.title = title
+    article.content = content
+    article.save()
+
+    # {% url 'articles:detail', id=article.id %}
+    return redirect('articles:detail', id=article.id)
